216e8f8e93714f480471bd9f0770771d09ffbd2f
ThomasMoulins <13thomasmoulins@gmail.com>
2024-10-22 10:46:02 +0200

    CHANGE update category with useLocation

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,15 +1,25 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import ProductCard from "./ProductCard";
 
 const Category = () => {
   const [products, setProducts] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(null);
-  const { category } = useParams();
+  const location = useLocation();
+
+  // Extraire la catégorie du chemin
+  const pathname = location.pathname;
+  const category = pathname.split("/category/")[1];
+
+  // Décoder la catégorie pour gérer les espaces et les caractères spéciaux
+  const decodedCategory = decodeURIComponent(category);
 
   useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/category/${category}?limit=5`)
+    setIsLoaded(false);
+    fetch(
+      `https://fakestoreapi.com/products/category/${decodedCategory}?limit=5`
+    )
       .then((res) => res.json())
       .then(
         (result) => {
@@ -23,16 +33,21 @@ const Category = () => {
           console.log(error);
         }
       );
-  }, []);
+  }, [decodedCategory]);
 
   if (error) {
     return <div>Erreur : {error.message}</div>;
   } else if (!isLoaded) {
-    return <div>Chargement...</div>;
+    return (
+      <>
+        <h1>{decodedCategory}</h1>
+        <div>Chargement...</div>
+      </>
+    );
   } else {
     return (
       <>
-        <h1>{category}</h1>
+        <h1>{decodedCategory}</h1>
         <ProductCard products={products} />
       </>
     );
